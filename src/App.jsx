fc217cdd689e53f3e0809ad34d8765fb45bb0ef9
Masum Billah <emasumbillah@gmail.com>
2024-05-30 19:54:54 +0800

    Another Commit2

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         marginTop: 10
         }}>
         <p style={{fontSize: 200}}>
-          Rajib Bhie, Thank You.
+          Hello!.
           </p>
         <button style={{
           backgroundColor: 'black', 
