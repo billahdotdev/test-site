da1db897378d5a36b2a650fe2e02e69bf157ee8b
Masum Billah <emasumbillah@gmail.com>
2024-07-02 10:40:06 +0800

    comitted again

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div
+                style={{
+                    backgroundColor: 'darkgreen',
+                    border: 2,
+                    borderRadius: 10,
+                    textAlign: 'center',
+                    margin: 10,
+                    padding: 10,
+                    marginTop: 10,
+                }}>
+                <p style={{ fontSize: 150, color: 'white' }}>HELLO!</p>
+                <button
+                    style={{
+                        backgroundColor: 'white',
+                        color: 'darkgreen',
+                        margin: 10,
+                        padding: 10,
+                        width: 100,
+                        height: 70,
+                        fontSize: 15,
+                    }}>
+                    Let's Click!!
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default App;
